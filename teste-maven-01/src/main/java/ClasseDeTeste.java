2b9d5eff5fe6ab6d77b67d89a75136a2c36ca063
MatosOliveira <matosalem.oliveira@gmail.com>
2016-09-15 10:49:27 -0700

    Inclusao da dependencia do Maven e criacao da ClasseDeTeste

---
+++ teste-maven-01/src/main/java/ClasseDeTeste.java
@@ -0,0 +1,22 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 
+ */
+
+/**
+ * @author matao01
+ *
+ */
+public class ClasseDeTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println(StringUtils.capitalize("matos"));
+
+	}
+
+}
